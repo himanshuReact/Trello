16344347ab94441153b2a6965ab87e565c23d788
Himanshu Shekhar <himanshu.shekhar@timeinc.com>
2020-12-02 14:01:17 +0530

    added the trello code

---
+++ src/utils/store.js
@@ -0,0 +1,37 @@
+const cards = [
+  {
+    id: 'card-1',
+    title: 'Learning how to cook',
+  },
+  {
+    id: 'card-2',
+    title: 'Making sandwich',
+  },
+  {
+    id: 'card-3',
+    title: 'Taking the trash out',
+  },
+];
+
+const data = {
+  lists: {
+    'list-1': {
+      id: 'list-1',
+      title: 'Todo',
+      cards,
+    },
+    'list-2': {
+      id: 'list-2',
+      title: 'Doing',
+      cards: [],
+    },
+    'list-3': {
+      id: 'list-3',
+      title: 'Done',
+      cards: [],
+    },
+  },
+  listIds: ['list-1', 'list-2', 'list-3'],
+};
+
+export default data;
